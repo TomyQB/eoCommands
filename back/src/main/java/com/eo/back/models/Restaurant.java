648a6e4a99255194f3d0bb1194de8659e12539cd
TomyQB <montialvo@gmail.com>
2021-10-11 18:49:00 +0200

    round total

--- back/src/main/java/com/eo/back/models/Restaurant.java
+++ back/src/main/java/com/eo/back/models/Restaurant.java
@@ -6,16 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "restaurant")
