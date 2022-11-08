12a9090f872e5ece2960f524a59f165e75de09c5
TomyQB <montialvo@gmail.com>
2022-11-08 11:15:47 +0100

    get and save printers

---
+++ back/src/main/java/com/eo/back/controllers/RestaurantPrinterController.java
@@ -0,0 +1,33 @@
+package com.eo.back.controllers;
+
+import java.util.List;
+
+import com.eo.back.dto.Restaurant.RestaurantPrinterDTO;
+import com.eo.back.models.RestaurantPrinter;
+import com.eo.back.services.RestaurantPrinterService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class RestaurantPrinterController {
+
+    @Autowired
+    private RestaurantPrinterService restaurantPrinterService;
+    
+    @GetMapping("/getPrinters/{restaurantId}")
+    public List<RestaurantPrinter> getPrinters(@PathVariable long restaurantId) {
+        return restaurantPrinterService.getRestaurantPrinters(restaurantId);
+    }
+
+    @PostMapping("/savePrinters/{restaurantId}")
+    public void savePrinters(@PathVariable long restaurantId, @RequestBody List<RestaurantPrinterDTO> restaurantPrinters) {
+        restaurantPrinterService.savePrinters(restaurantId, restaurantPrinters);
+    }
+}
