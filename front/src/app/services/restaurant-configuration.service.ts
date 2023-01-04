d79d4076dbcd19de9e8c1135473689447b49ba66
manuel <1198manuel@gmail.com>
2023-01-04 20:15:01 +0100

    chore: change all logic to tab-cuenta

--- front/src/app/services/restaurant-configuration.service.ts
+++ front/src/app/services/restaurant-configuration.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.prod';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RestaurantConfigurationService {
   Url = environment.Url;
-  restaurantConfig =  JSON.parse(sessionStorage.getItem('restaurantConfig')!)
-  
+  restaurantConfigObservable: BehaviorSubject<any> = new BehaviorSubject<any>(
+    null
+  );
+  restaurantConfig = JSON.parse(sessionStorage.getItem('restaurantConfig')!);
+
   constructor(private http: HttpClient) {}
+
+  // getRestaurantConfigObservable() {
+  //   return this.restaurantConfigObservable();
+  // }
+
+  setSharingObservableData(data: any) {
+    this.restaurantConfigObservable.next(data);
+  }
 
   getConfiguration(restaurantId: number) {
     return this.http.get<any>(this.Url + 'getConfig/' + restaurantId);
