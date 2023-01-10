16ea3b37c239905910adbf713a394d2c3bd84e53
manuel <1198manuel@gmail.com>
2023-01-10 21:54:21 +0100

    chore: separar mesa

--- front/src/app/components/tabs/tab-cuenta/separar-mesa/separar-mesa.component.ts
+++ front/src/app/components/tabs/tab-cuenta/separar-mesa/separar-mesa.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -15,26 +14,52 @@ import { CurrencySumbolService } from '../../../../services/currency-sumbol.serv
 export class SepararMesaComponent implements OnInit {
   cuentaDividida = <any>[];
   cuentaCompleta = <any>[];
-
+  cuenta = [];
   constructor(
     private pedidoServices: PedidoServicesService,
     public currencySumbolService: CurrencySumbolService
-  ) {
-    this.cuentaCompleta = pedidoServices.pedido;
-  }
+  ) {}
 
   ngOnInit(): void {
+    this.cuenta = JSON.parse(
+      JSON.stringify(this.pedidoServices.pedidosParaSeparar)
+    );
+
+    this.cuenta.forEach((pedido: any) => {
+      this.cuentaCompleta = this.cuentaCompleta.concat(pedido.amounts);
+    });
+
     this.fillCuenta();
   }
 
   fillCuenta() {
     for (let i = 0; this.cuentaCompleta.length > i; i++) {
+      let extras: any = [];
       if (this.cuentaCompleta[i].amount > 1) {
+        this.cuentaCompleta[i].extras.forEach((extra: any) => {
+          if (extra.amount > 1) {
+            extra.amount--;
+            extras.push(extra);
+          }
+        });
         this.cuentaCompleta.push({
           ...this.cuentaCompleta[i],
           amount: this.cuentaCompleta[i].amount - 1,
+          extras,
         });
-        this.cuentaCompleta[i].amount = 1;
+        this.cuentaCompleta[i].amount--;
+      }
+
+      if (this.cuentaCompleta[i].plate) {
+        let subTotal =
+          this.cuentaCompleta[i].amount * this.cuentaCompleta[i].plate.price;
+
+        this.cuentaCompleta[i].extras.forEach((extra: any) => {
+          subTotal = extra.price + subTotal;
+        });
+        subTotal = Math.round((subTotal + Number.EPSILON) * 100) / 100;
+
+        this.cuentaCompleta[i].subTotal = subTotal;
       }
     }
   }
@@ -97,8 +122,7 @@ export class SepararMesaComponent implements OnInit {
   total() {
     let total = 0;
     this.cuentaCompleta.map((plato: any) => {
-      total =
-        total + (plato.plate ? plato.plate.price : plato.additional.price);
+      total = total + plato.subTotal;
     });
     total = Math.round((total + Number.EPSILON) * 100) / 100;
     return total;
@@ -107,8 +131,7 @@ export class SepararMesaComponent implements OnInit {
   totalDividida() {
     let total = 0;
     this.cuentaDividida.map((plato: any) => {
-      total =
-        total + (plato.plate ? plato.plate.price : plato.additional.price);
+      total = total + plato.subTotal;
     });
     total = Math.round((total + Number.EPSILON) * 100) / 100;
     return total;
